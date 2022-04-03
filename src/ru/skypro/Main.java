cc32e2ff1a3b668ae70609586032aede825d7d2b
aktov <aktovi@gmail.com>
2022-04-03 08:41:49 +0300

    Homework_1.2_hard

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,7 +3,31 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
+        //Для каждой задачачи из-за повторяющихся названий переменных, необходимо снимать комментарии
+
+        //Задача  6
+//        int a = 12;
+//        int b = 27;
+//        int c = 44;
+//        int d = 15;
+//        int e = 9;
+//        int result = a * (b + (c - d * e));
+//        result = - result;
+//        System.out.println("invertion result is " + result);
+
+
+        //Задача 7
+//        int a = 5;
+//        int b = 7;
+//        a = a + b;
+//        b = a - b;
+//        a = a - b;
+//        System.out.println("a = " + a + " b = " + b);
+
+        //Задача 8
+        int a = 256;
+        int b = a % 100 / 10;
+        System.out.println("b = " + b);
     }
 }
